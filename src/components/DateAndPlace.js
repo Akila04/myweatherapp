9de9a1ace146217af25abac000c130e40c9a34ff
Akila04 <67583026+Akila04@users.noreply.github.com>
2020-08-05 19:41:51 +0530

    Update DateAndPlace.js

--- src/components/DateAndPlace.js
+++ src/components/DateAndPlace.js
@@ -5,7 +5,7 @@ class DateAndPlace extends Component{
 	getdate = (date) =>{
 
 		if(date != ''){
-			const timezone1=new Date(parseInt(date) * 1000).toString();
+			const timezone1=new Date(parseInt('1596636058') * 1000).toString();
 			const date=timezone1.substring(0,16);
 			return <p className="date">{date}</p>
 		}
